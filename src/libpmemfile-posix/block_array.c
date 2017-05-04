5d2939b39d1800fc81de79441b65a5225420a08d
Gabor Buella <gabor.buella@intel.com>
2017-05-04 10:03:51 +0200

    posix: block_array.c descriptions

--- src/libpmemfile-posix/block_array.c
+++ src/libpmemfile-posix/block_array.c
@@ -37,6 +37,19 @@
 #include "out.h"
 #include "block_array.h"
 
+/*
+ * update_first_block_info
+ * The vinode structs contain some precomputed information about the block
+ * arrays in the file. This routine updates these if needed.
+ * Upon opening a file (creating a new vinode), the binfo->arr pointer
+ * is set to NULL. This is checked for in the `binfo->arr != NULL` condition.
+ * Every routine dealing directly with block allocation (two of them) calls
+ * this routine before using the block_info struct, thus lazy-initializing it.
+ * If new blocks are never allocated, and existing blocks are never removed
+ * during the lifetime of a vinode, this initialization never happens.
+ * Once this initialization happens, the allocating routines are expected
+ * to keep this data up to date.
+ */
 static void
 update_first_block_info(struct pmemfile_vinode *vinode)
 {
@@ -75,6 +88,12 @@ update_first_block_info(struct pmemfile_vinode *vinode)
 		binfo->idx++;
 }
 
+/*
+ * has_free_block_entry - is there a free slot in the already
+ *  allocated block_arrays to hold another block's metadata?
+ *
+ * If not, a new block_array should be allocated.
+ */
 static bool
 has_free_block_entry(struct pmemfile_vinode *vinode)
 {
@@ -83,9 +102,57 @@ has_free_block_entry(struct pmemfile_vinode *vinode)
 	return binfo->idx < binfo->arr->length;
 }
 
+/*
+ * allocate_new_block_array
+ * Allocates a new block_array, and links it into the linked list of
+ * block_arrays associated with the file. It is assumed, that there is no free
+ * slot available in any of the existing block_arrays - hence the need to
+ * allocate a new one.
+ *
+ * The new block_array is linked in as the first item into the linked list.
+ * There is always a zeroth item, the one stored internally in struct inode.
+ * See the layout.h header file about that.
+ *
+ * Before (0th block array is full, 1st block array is full):
+ * +--------------------------+
+ * | struct pmemfile_inode    |
+ * |     +-----------------+  |   +------------------+
+ * |     | 0th block array |  |   | 1st block array  |
+ * |     |    next->-------+--+-->|     next->       |
+ * |     |---------------- |  |   | ---------------- |
+ * |     ||b |b |b |b |b | |  |   | |b |b |b |b |b | |
+ * |     |---------------- |  |   | ---------------- |
+ * |     +-----------------+  |   +------------------+
+ * |                          |
+ * +--------------------------+                   ||
+ *                                                ||
+ *                                                \\ old 1st block array becomes
+ *                                                 \\ the new 2nd block array
+ *                                                  \\
+ *                                                   \\
+ * After:                                             \\
+ * (0th and 2nd block arrays are full,                 \\
+ * 1st block array is empty)                            \\
+ * +--------------------------+                          \\
+ * | struct pmemfile_inode    |
+ * |     +-----------------+  |   +------------------+  +------------------+
+ * |     | 0th block array |  |   | new block array  |  | 2nd block array  |
+ * |     |    next->-------+--+-->|     next->-------+->|     next->       |
+ * |     |---------------- |  |   | ---------------- |  | ---------------- |
+ * |     ||b |b |b |b |b | |  |   | |  |  |  |  |  | |  | |b |b |b |b |b | |
+ * |     |---------------- |  |   | --^------------- |  | ---------------- |
+ * |     +-----------------+  |   +---^--------------+  +------------------+
+ * |                          |       ^
+ * +--------------------------+       ^
+ *                                    ^
+ *                                    |
+ *                 The next free slot for block metadata
+ */
 static void
 allocate_new_block_array(struct pmemfile_vinode *vinode)
 {
+	ASSERT(!has_free_block_entry(vinode));
+
 	TOID(struct pmemfile_block_array) new =
 			TX_ZALLOC(struct pmemfile_block_array, FILE_PAGE_SIZE);
 	D_RW(new)->length = (uint32_t)
@@ -113,6 +180,29 @@ is_zeroed(const void *addr, size_t len)
 	return true;
 }
 
+/*
+ * acquire_new_entry
+ * Finds a new slot for block metadata inside a block_array associated
+ * with the file. The array itself does not store the number of elements
+ * already used, this is only tracked in the vinode:
+ * the field vinode->first_free_block->idx is the index of the first free
+ * element. This routine simply increases this index. As long as this index
+ * is smaller than the number of elements available (block_array->length),
+ * there is no need to allocate new block_arrays from pmemobj.
+ * See also the has_free_block_entry function above.
+ *
+ *
+ * +------------------+
+ * | block array      |
+ * | ---------------- |
+ * | |b |b |  |  |  | |
+ * | -------^-------- |
+ * +--------^---------+
+ *          ^
+ *          ^
+ *          \__return the address of this pmemfile_block
+ *
+ */
 static struct pmemfile_block *
 acquire_new_entry(struct pmemfile_vinode *vinode)
 {
@@ -134,10 +224,20 @@ acquire_new_entry(struct pmemfile_vinode *vinode)
 	return block;
 }
 
+/*
+ * block_list_insert_after
+ * Finds a free slot in the block_arrays associated with the file (allocate
+ * a new block_array if needed).
+ * Links the block into the linked list of blocks right after the already
+ * existing block supplied in the 'prev' argument.
+ * Returns a pointer to the free slot found.
+ *
+ */
 struct pmemfile_block *
 block_list_insert_after(struct pmemfile_vinode *vinode,
 			struct pmemfile_block *prev)
 {
+	/* lazy init vinode->first_free_block */
 	update_first_block_info(vinode);
 
 	struct pmemfile_block *block = acquire_new_entry(vinode);
@@ -161,6 +261,11 @@ block_list_insert_after(struct pmemfile_vinode *vinode,
 	return block;
 }
 
+/*
+ * last_used_block
+ * Returns a pointer to last (last in terms of allocated most recently) block
+ * metadata. This is always the block right before the first free block.
+ */
 static struct pmemfile_block *
 last_used_block(struct pmemfile_vinode *vinode)
 {
@@ -171,6 +276,10 @@ last_used_block(struct pmemfile_vinode *vinode)
 	return binfo->arr->blocks + (binfo->idx - 1);
 }
 
+/*
+ * unlink_block - removes the block metadata from the linked list of blocks
+ * Note: does not deallocate the block metadata, only unlinks it.
+ */
 static void
 unlink_block(struct pmemfile_block *block)
 {
@@ -181,6 +290,17 @@ unlink_block(struct pmemfile_block *block)
 		TX_SET(block->next, prev, block->prev);
 }
 
+/*
+ * relocate_block - overwrite *dst with *src
+ * The block metadata at src is moved to the place at dst - while also updating
+ * the appropriate prev and next pmem pointers to keep track of the new
+ * location of the relocated block metadata.
+ *
+ * Whatever was at *dst, is discarded.
+ *
+ * Does not alter the data at *src, but no other pmemfile_block points to it
+ * after this operation.
+ */
 static void
 relocate_block(struct pmemfile_block *dst, struct pmemfile_block *src)
 {
@@ -197,18 +317,33 @@ relocate_block(struct pmemfile_block *dst, struct pmemfile_block *src)
 	TX_MEMCPY(dst, src, sizeof(*src));
 }
 
+/*
+ * is_first_block_array_empty
+ * Is the first block_array empty?
+ * Is this block_array not the one stored inside the pmemfile_vinode struct?
+ */
 static bool
-must_remove_a_block_array(struct pmemfile_vinode *vinode)
+is_first_block_array_empty(struct pmemfile_vinode *vinode)
 {
 	if (vinode->first_free_block.idx != 0)
-		return false;
+		return false; /* it is not empty */
 
+	/*
+	 * Is this the one stored inside the vinode?
+	 * If yes then in a sense it is the zeroth block array, not the first.
+	 */
 	return vinode->first_free_block.arr !=
 	    &vinode->inode->file_data.blocks;
 }
 
+/*
+ * remove_first_block_array - unlinks a block_array from the linked list
+ *  of block arrays, and releases the memory using TX_FREE
+ *
+ * Also: updates the vinode->first_free_block data structure as needed.
+ */
 static void
-remove_a_block_array(struct pmemfile_vinode *vinode)
+remove_first_block_array(struct pmemfile_vinode *vinode)
 {
 	TOID(struct pmemfile_block_array) to_remove;
 	TOID(struct pmemfile_block_array) new_next;
@@ -229,12 +364,68 @@ remove_a_block_array(struct pmemfile_vinode *vinode)
 	binfo->idx = binfo->arr->length;
 }
 
+/*
+ * block_list_remove - remove a pmemfile_block item, and deallocate the block
+ *  data associated with it
+ *
+ * This routine makes sure an invariant is held: all the free pmemfile_block
+ * slots associated with this file are in a single block_array.
+ * In order to achieve this, it must sometimes relocate one block to fill an
+ * empty slot appearing after removing a block.
+ *
+ * Diagram with 11 blocks numbered hexadecimally, b0 - bb, removing block b6:
+ *
+ * Before removing b6 (invariant held):
+ *
+ * +--------------------------+
+ * | struct pmemfile_inode    |
+ * |     +-----------------+  |   +------------------+  +------------------+
+ * |     | 0th block array |  |   | 1st block array  |  | 2nd block array  |
+ * |     |    next->-------+--+-->|     next->-------+->|     next->       |
+ * |     |---------------- |  |   | ---------------- |  | ---------------- |
+ * |     ||b0|b1|b2|b3|b4| |  |   | |ba|bb|  |  |  | |  | |b5|b6|b7|b8|b9| |
+ * |     |---------------- |  |   | -------^-------- |  | ---------------- |
+ * |     +-----------------+  |   +--------^---------+  +------------------+
+ * |                          |            ^
+ * +--------------------------+            \__free slots
+ *
+ * After removing b6 (invariant not held):
+ *
+ * +--------------------------+
+ * | struct pmemfile_inode    |
+ * |     +-----------------+  |   +------------------+  +------------------+
+ * |     | 0th block array |  |   | 1st block array  |  | 2nd block array  |
+ * |     |    next->-------+--+-->|     next->-------+->|     next->       |
+ * |     |---------------- |  |   | ---------------- |  | ---------------- |
+ * |     ||b0|b1|b2|b3|b4| |  |   | |ba|bb|  |  |  | |  | |b5|  |b7|b8|b9| |
+ * |     |---------------- |  |   | -------^-------- |  | ----^----------- |
+ * |     +-----------------+  |   +--------^---------+  +-----^------------+
+ * |                          |            ^                  ^
+ * +--------------------------+            \__free slots      \__new free slot
+ *
+ * Restoring the invariant (filling the new free slot with bb):
+ *
+ * +--------------------------+
+ * | struct pmemfile_inode    |
+ * |     +-----------------+  |   +------------------+  +------------------+
+ * |     | 0th block array |  |   | 1st block array  |  | 2nd block array  |
+ * |     |    next->-------+--+-->|     next->-------+->|     next->       |
+ * |     |---------------- |  |   | ---------------- |  | ---------------- |
+ * |     ||b0|b1|b2|b3|b4| |  |   | |ba|  |  |  |  | |  | |b5|bb|b7|b8|b9| |
+ * |     |---------------- |  |   | ----^----------- |  | ----^----------- |
+ * |     +-----------------+  |   +-----^------------+  +-----^------------+
+ * |                          |         ^                     ^
+ * +--------------------------+         \__free slots         \__relocated block
+ *
+ *
+ */
 struct pmemfile_block *
 block_list_remove(struct pmemfile_vinode *vinode,
 		struct pmemfile_block *block)
 {
 	struct pmemfile_block *prev;
 
+	/* lazy init vinode->first_free_block */
 	update_first_block_info(vinode);
 
 	ASSERT(vinode->first_free_block.idx > 0);
@@ -267,8 +458,8 @@ block_list_remove(struct pmemfile_vinode *vinode,
 
 	vinode->first_free_block.idx--;
 
-	if (must_remove_a_block_array(vinode))
-		remove_a_block_array(vinode);
+	if (is_first_block_array_empty(vinode))
+		remove_first_block_array(vinode);
 
 	return prev;
 }

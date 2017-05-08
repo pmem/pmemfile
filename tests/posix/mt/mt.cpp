04a5400cd17fc5580a93354239d63e1057474a5d
Marcin Ślusarz <marcin.slusarz@intel.com>
2017-05-08 17:04:52 +0200

    tests: add multi-threaded rename test

--- tests/posix/mt/mt.cpp
+++ tests/posix/mt/mt.cpp
@@ -170,6 +170,125 @@ TEST_F(mt, pread)
 	ASSERT_EQ(pmemfile_unlink(pfp, "/file1"), 0);
 }
 
+static void
+test_rename(const char *path1, const char *path2)
+{
+	pmemfile_rename(global_pfp, path1, path2);
+	pmemfile_rename(global_pfp, path2, path1);
+}
+
+static void
+test_rename_loop(const char *path1, const char *path2)
+{
+	for (int i = 0; i < ops; ++i)
+		test_rename(path1, path2);
+}
+
+static void
+test_exchange(const char *path1, const char *path2)
+{
+	pmemfile_renameat2(global_pfp, NULL, path1, NULL, path2,
+			   PMEMFILE_RENAME_EXCHANGE);
+	pmemfile_renameat2(global_pfp, NULL, path2, NULL, path1,
+			   PMEMFILE_RENAME_EXCHANGE);
+}
+
+static void
+test_exchange_loop(const char *path1, const char *path2)
+{
+	for (int i = 0; i < ops; ++i)
+		test_exchange(path1, path2);
+}
+
+/* same-directory file renames */
+static void
+rename_worker1(void)
+{
+	test_rename_loop("/dir1/file1", "/dir1/file11");
+}
+
+/*
+ * same-directory file renames (other file in the same directory as previous
+ * one)
+ */
+static void
+rename_worker2(void)
+{
+	test_rename_loop("/dir1/file2", "/dir1/file21");
+}
+
+/* cross-directory file renames */
+static void
+rename_worker3(void)
+{
+	test_rename_loop("/dir2/file1", "/dir3/file11");
+}
+
+/*
+ * cross-directory file renames (other file in the same directory as previous
+ * one)
+ */
+static void
+rename_worker4(void)
+{
+	test_rename_loop("/dir2/file2", "/dir3/file22");
+}
+
+/* cross-directory directory renames */
+static void
+rename_worker5(void)
+{
+	test_rename_loop("/dir4/dir1", "/dir4/dir2/dir6");
+}
+
+/* cross-directory directory-file exchange */
+static void
+rename_worker6(void)
+{
+	test_exchange_loop("/dir4/dir3", "/dir4/dir2/file4");
+}
+
+TEST_F(mt, rename)
+{
+	test_empty_dir_on_teardown = false;
+	ASSERT_EQ(pmemfile_mkdir(pfp, "/dir1", 0755), 0);
+	ASSERT_EQ(pmemfile_mkdir(pfp, "/dir2", 0755), 0);
+	ASSERT_EQ(pmemfile_mkdir(pfp, "/dir3", 0755), 0);
+
+	ASSERT_EQ(pmemfile_mkdir(pfp, "/dir4", 0755), 0);
+	ASSERT_EQ(pmemfile_mkdir(pfp, "/dir4/dir1", 0755), 0);
+	ASSERT_EQ(pmemfile_mkdir(pfp, "/dir4/dir2", 0755), 0);
+	ASSERT_EQ(pmemfile_mkdir(pfp, "/dir4/dir3", 0755), 0);
+	ASSERT_EQ(pmemfile_mkdir(pfp, "/dir4/dir1/dir5", 0755), 0);
+	ASSERT_EQ(pmemfile_mkdir(pfp, "/dir4/dir2/dir6", 0755), 0);
+	ASSERT_EQ(pmemfile_mkdir(pfp, "/dir4/dir3/dir7", 0755), 0);
+
+	ASSERT_TRUE(test_pmemfile_create(pfp, "/dir4/file1"));
+	ASSERT_TRUE(test_pmemfile_create(pfp, "/dir4/dir1/file2"));
+	ASSERT_TRUE(test_pmemfile_create(pfp, "/dir4/dir1/dir5/file3"));
+	ASSERT_TRUE(test_pmemfile_create(pfp, "/dir4/dir2/file4"));
+	ASSERT_TRUE(test_pmemfile_create(pfp, "/dir4/dir2/dir6/file5"));
+	ASSERT_TRUE(test_pmemfile_create(pfp, "/dir4/dir3/file6"));
+	ASSERT_TRUE(test_pmemfile_create(pfp, "/dir4/dir3/dir7/file8"));
+
+	ASSERT_TRUE(test_pmemfile_create(pfp, "/dir1/file1"));
+	ASSERT_TRUE(test_pmemfile_create(pfp, "/dir1/file2"));
+	ASSERT_TRUE(test_pmemfile_create(pfp, "/dir2/file1"));
+	ASSERT_TRUE(test_pmemfile_create(pfp, "/dir2/file2"));
+
+	for (int i = 0; i < 2; ++i) {
+		threads.emplace_back(rename_worker1);
+		threads.emplace_back(rename_worker2);
+		threads.emplace_back(rename_worker3);
+		threads.emplace_back(rename_worker4);
+		threads.emplace_back(rename_worker5);
+		threads.emplace_back(rename_worker6);
+	}
+
+	for (auto &t : threads)
+		t.join();
+}
+
 int
 main(int argc, char *argv[])
 {

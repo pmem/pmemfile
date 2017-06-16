87d856d86d6b57c36669203fe8b4a1b3913748a5
Marcin Ślusarz <marcin.slusarz@intel.com>
2017-06-16 11:26:19 +0200

    posix: implement timestamp management API
    Ref: pmem/pmemfile#175

--- include/libpmemfile-posix-stubs.h
+++ include/libpmemfile-posix-stubs.h
@@ -65,24 +65,6 @@ void *pmemfile_mremap(PMEMfilepool *, void *old_addr, size_t old_size,
 int pmemfile_msync(PMEMfilepool *, void *addr, size_t len, int flags);
 int pmemfile_mprotect(PMEMfilepool *, void *addr, size_t len, int prot);
 
-typedef struct utimbuf pmemfile_utimbuf_t;
-
-int pmemfile_utime(PMEMfilepool *, const char *filename,
-		const pmemfile_utimbuf_t *times);
-
-typedef struct timeval pmemfile_timeval_t;
-
-int pmemfile_utimes(PMEMfilepool *, const char *filename,
-		const pmemfile_timeval_t times[2]);
-int pmemfile_futimes(PMEMfilepool *, PMEMfile *file,
-		const pmemfile_timeval_t tv[2]);
-int pmemfile_lutimes(PMEMfilepool *, const char *filename,
-		const pmemfile_timeval_t tv[2]);
-int pmemfile_utimensat(PMEMfilepool *, PMEMfile *dir, const char *pathname,
-		const pmemfile_timespec_t times[2], int flags);
-int pmemfile_futimens(PMEMfilepool *, PMEMfile *file,
-		const pmemfile_timespec_t times[2]);
-
 pmemfile_mode_t pmemfile_umask(PMEMfilepool *, pmemfile_mode_t mask);
 
 pmemfile_ssize_t pmemfile_copy_file_range(PMEMfilepool *,
--- src/libpmemfile-posix/timestamps.c
+++ src/libpmemfile-posix/timestamps.c
@@ -35,73 +35,386 @@
  */
 
 #include <errno.h>
+#include "callbacks.h"
+#include "creds.h"
+#include "dir.h"
+#include "file.h"
 #include "libpmemfile-posix.h"
 #include "out.h"
+#include "pool.h"
+#include "utils.h"
+
+enum utime_macros { UTIME_MACROS_DISABLED, UTIME_MACROS_ENABLED };
+
+static int
+vinode_file_time_set(PMEMfilepool *pfp, struct pmemfile_vinode *vinode,
+		const struct pmemfile_time *tm, enum utime_macros utm)
+{
+	struct pmemfile_time tm_buf[2];
+	if (!tm) {
+		tm = tm_buf;
+
+		if (get_current_time(&tm_buf[0]))
+			return -1;
+
+		tm_buf[1] = tm_buf[0];
+
+		tm = tm_buf;
+	} else if (utm == UTIME_MACROS_ENABLED) {
+		memcpy(tm_buf, tm, 2 * sizeof(*tm));
+
+		for (int i = 0; i < 2; ++i) {
+			if (tm_buf[i].nsec == PMEMFILE_UTIME_NOW) {
+				if (get_current_time(&tm_buf[i]))
+					return -1;
+			} else if (tm_buf[i].nsec == PMEMFILE_UTIME_OMIT) {
+				/* nothing */
+			} else if (tm_buf[i].nsec < 0 ||
+					tm_buf[i].nsec > 999999999 ||
+					tm_buf[i].sec < 0) {
+				errno = EINVAL;
+				return -1;
+			}
+		}
+
+		tm = tm_buf;
+	} else {
+		for (int i = 0; i < 2; ++i) {
+			if (tm[i].nsec < 0 || tm[i].nsec > 999999999 ||
+					tm[i].sec < 0) {
+				errno = EINVAL;
+				return -1;
+			}
+		}
+	}
+
+	os_rwlock_wrlock(&vinode->rwlock);
+
+	int error = 0;
+	struct pmemfile_inode *inode = vinode->inode;
+
+	TX_BEGIN_CB(pfp->pop, cb_queue, pfp) {
+		if (utm == UTIME_MACROS_DISABLED ||
+				tm[0].nsec != PMEMFILE_UTIME_OMIT) {
+			TX_ADD_DIRECT(&inode->atime);
+			inode->atime = tm[0];
+		}
+
+		if (utm == UTIME_MACROS_DISABLED ||
+				tm[1].nsec != PMEMFILE_UTIME_OMIT) {
+			TX_ADD_DIRECT(&inode->mtime);
+			inode->mtime = tm[1];
+		}
+	} TX_ONABORT {
+		error = errno;
+	} TX_END
+
+	os_rwlock_unlock(&vinode->rwlock);
+
+	if (error) {
+		errno = error;
+		return -1;
+	}
+
+	return 0;
+}
+
+static int
+pmemfile_file_time_set(PMEMfilepool *pfp, PMEMfile *dir, const char *filename,
+		const struct pmemfile_time *tm,
+		enum symlink_resolve last_symlink, enum utime_macros utm)
+{
+	if (!pfp) {
+		LOG(LUSR, "NULL pool");
+		errno = EFAULT;
+		return -1;
+	}
+
+	if (!filename) {
+		LOG(LUSR, "NULL filename");
+		errno = ENOENT;
+		return -1;
+	}
+
+	if (filename[0] != '/' && !dir) {
+		LOG(LUSR, "NULL dir");
+		errno = EFAULT;
+		return -1;
+	}
+
+	/*
+	 * From man utimensat:
+	 * "If both tv_nsec fields are specified as UTIME_OMIT, then the Linux
+	 * implementation of utimensat() succeeds even if the file referred to
+	 * by dirfd and pathname does not exist."
+	 */
+	if (utm == UTIME_MACROS_ENABLED &&
+			tm[0].nsec == PMEMFILE_UTIME_OMIT &&
+			tm[1].nsec == PMEMFILE_UTIME_OMIT) {
+		return 0;
+	}
+
+	struct pmemfile_cred cred;
+	if (cred_acquire(pfp, &cred))
+		return -1;
+
+	int error = 0;
+	bool at_unref;
+
+	struct pmemfile_vinode *at =
+			pool_get_dir_for_path(pfp, dir, filename, &at_unref);
+
+	struct pmemfile_path_info info;
+	struct pmemfile_vinode *vinode = resolve_pathat_full(pfp, &cred, at,
+			filename, &info, 0, last_symlink);
+
+	if (info.error) {
+		error = info.error;
+		goto end;
+	}
+
+	if (!_vinode_can_access(&cred, vinode, PFILE_WANT_WRITE)) {
+		error = EACCES;
+		goto end;
+	}
+
+	if (vinode_file_time_set(pfp, vinode, tm, utm))
+		error = errno;
+
+end:
+	path_info_cleanup(pfp, &info);
+	cred_release(&cred);
+
+	ASSERT_NOT_IN_TX();
+	if (vinode)
+		vinode_unref(pfp, vinode);
+
+	if (at_unref)
+		vinode_cleanup(pfp, at, error != 0);
+
+	if (error) {
+		errno = error;
+		return -1;
+	}
+
+	return 0;
+}
 
 int
 pmemfile_utime(PMEMfilepool *pfp, const char *filename,
 		const pmemfile_utimbuf_t *times)
 {
-	(void) filename;
-	(void) times;
+	if (!times)
+		return pmemfile_file_time_set(pfp, PMEMFILE_AT_CWD, filename,
+				NULL, RESOLVE_LAST_SYMLINK,
+				UTIME_MACROS_DISABLED);
 
-	errno = ENOTSUP;
-	return -1;
+	struct pmemfile_time tm[2];
+
+	tm[0].sec = times->actime;
+	tm[0].nsec = 0;
+	tm[1].sec = times->modtime;
+	tm[1].nsec = 0;
+
+	return pmemfile_file_time_set(pfp, PMEMFILE_AT_CWD, filename, tm,
+			RESOLVE_LAST_SYMLINK, UTIME_MACROS_DISABLED);
 }
 
 int
 pmemfile_utimes(PMEMfilepool *pfp, const char *filename,
 		const pmemfile_timeval_t times[2])
 {
-	(void) filename;
-	(void) times;
+	if (!times)
+		return pmemfile_file_time_set(pfp, PMEMFILE_AT_CWD, filename,
+				NULL, RESOLVE_LAST_SYMLINK,
+				UTIME_MACROS_DISABLED);
 
-	errno = ENOTSUP;
-	return -1;
+	struct pmemfile_time tm[2];
+
+	tm[0].sec = times[0].tv_sec;
+	tm[0].nsec = times[0].tv_usec * 1000;
+	tm[1].sec = times[1].tv_sec;
+	tm[1].nsec = times[1].tv_usec * 1000;
+
+	return pmemfile_file_time_set(pfp, PMEMFILE_AT_CWD, filename, tm,
+			RESOLVE_LAST_SYMLINK, UTIME_MACROS_DISABLED);
 }
 
 int
 pmemfile_futimes(PMEMfilepool *pfp, PMEMfile *file,
 		const pmemfile_timeval_t tv[2])
 {
-	(void) file;
-	(void) tv;
+	if (!pfp) {
+		LOG(LUSR, "NULL pool");
+		errno = EFAULT;
+		return -1;
+	}
 
-	errno = ENOTSUP;
-	return -1;
+	if (!file) {
+		LOG(LUSR, "NULL file");
+		errno = EFAULT;
+		return -1;
+	}
+
+	uint64_t flags;
+
+	os_mutex_lock(&file->mutex);
+	flags = file->flags;
+	os_mutex_unlock(&file->mutex);
+
+	if (!(flags & PFILE_WRITE)) {
+		errno = EBADF;
+		return -1;
+	}
+
+	if (!tv)
+		return vinode_file_time_set(pfp, file->vinode, NULL,
+				UTIME_MACROS_DISABLED);
+
+	struct pmemfile_time tm[2];
+
+	tm[0].sec = tv[0].tv_sec;
+	tm[0].nsec = tv[0].tv_usec * 1000;
+	tm[1].sec = tv[1].tv_sec;
+	tm[1].nsec = tv[1].tv_usec * 1000;
+
+	return vinode_file_time_set(pfp, file->vinode, tm,
+			UTIME_MACROS_DISABLED);
 }
 
 int
 pmemfile_lutimes(PMEMfilepool *pfp, const char *filename,
 		const pmemfile_timeval_t tv[2])
 {
-	(void) filename;
-	(void) tv;
+	if (!tv)
+		return pmemfile_file_time_set(pfp, PMEMFILE_AT_CWD, filename,
+				NULL, NO_RESOLVE_LAST_SYMLINK,
+				UTIME_MACROS_DISABLED);
 
-	errno = ENOTSUP;
-	return -1;
+	struct pmemfile_time tm[2];
+
+	tm[0].sec = tv[0].tv_sec;
+	tm[0].nsec = tv[0].tv_usec * 1000;
+	tm[1].sec = tv[1].tv_sec;
+	tm[1].nsec = tv[1].tv_usec * 1000;
+
+	return pmemfile_file_time_set(pfp, PMEMFILE_AT_CWD, filename, tm,
+			NO_RESOLVE_LAST_SYMLINK, UTIME_MACROS_DISABLED);
+}
+
+int
+pmemfile_futimesat(PMEMfilepool *pfp, PMEMfile *dir, const char *pathname,
+		const pmemfile_timeval_t tv[2])
+{
+	if (!tv)
+		return pmemfile_file_time_set(pfp, dir, pathname, NULL,
+				RESOLVE_LAST_SYMLINK, UTIME_MACROS_DISABLED);
+
+	struct pmemfile_time tm[2];
+
+	tm[0].sec = tv[0].tv_sec;
+	tm[0].nsec = tv[0].tv_usec * 1000;
+	tm[1].sec = tv[1].tv_sec;
+	tm[1].nsec = tv[1].tv_usec * 1000;
+
+	return pmemfile_file_time_set(pfp, dir, pathname, tm,
+			RESOLVE_LAST_SYMLINK, UTIME_MACROS_DISABLED);
 }
 
 int
 pmemfile_utimensat(PMEMfilepool *pfp, PMEMfile *dir, const char *pathname,
 		const pmemfile_timespec_t times[2], int flags)
 {
-	(void) dir;
-	(void) pathname;
-	(void) times;
-	(void) flags;
+	if (flags & ~PMEMFILE_AT_SYMLINK_NOFOLLOW) {
+		errno = EINVAL;
+		return -1;
+	}
 
-	errno = ENOTSUP;
-	return -1;
+	enum symlink_resolve last_symlink =
+			(flags & PMEMFILE_AT_SYMLINK_NOFOLLOW) ?
+			NO_RESOLVE_LAST_SYMLINK : RESOLVE_LAST_SYMLINK;
+
+	if (!times) {
+		/*
+		 * Linux nonstandard syscall-level feature. Glibc behaves
+		 * differently, but we have to emulate kernel behavior because
+		 * futimens at glibc level is implemented using utimensat with
+		 * NULL pathname. See "C library/ kernel ABI differences"
+		 * section in man utimensat.
+		 */
+		if (pathname == NULL) {
+			if (!dir || dir == PMEMFILE_AT_CWD) {
+				errno = EFAULT;
+				return -1;
+			}
+
+			return vinode_file_time_set(pfp, dir->vinode, NULL,
+					UTIME_MACROS_DISABLED);
+		}
+		return pmemfile_file_time_set(pfp, dir, pathname, NULL,
+				last_symlink, UTIME_MACROS_DISABLED);
+	}
+
+	struct pmemfile_time tm[2];
+
+	tm[0].sec = times[0].tv_sec;
+	tm[0].nsec = times[0].tv_nsec;
+	tm[1].sec = times[1].tv_sec;
+	tm[1].nsec = times[1].tv_nsec;
+
+	/* see comment above */
+	if (pathname == NULL) {
+		if (!dir || dir == PMEMFILE_AT_CWD) {
+			errno = EFAULT;
+			return -1;
+		}
+
+		return vinode_file_time_set(pfp, dir->vinode, tm,
+				UTIME_MACROS_ENABLED);
+	}
+
+	return pmemfile_file_time_set(pfp, dir, pathname, tm, last_symlink,
+			UTIME_MACROS_ENABLED);
 }
 
 int
 pmemfile_futimens(PMEMfilepool *pfp, PMEMfile *file,
 		const pmemfile_timespec_t times[2])
 {
-	(void) file;
-	(void) times;
+	if (!pfp) {
+		LOG(LUSR, "NULL pool");
+		errno = EFAULT;
+		return -1;
+	}
 
-	errno = ENOTSUP;
-	return -1;
+	if (!file) {
+		LOG(LUSR, "NULL file");
+		errno = EFAULT;
+		return -1;
+	}
+
+	uint64_t flags;
+
+	os_mutex_lock(&file->mutex);
+	flags = file->flags;
+	os_mutex_unlock(&file->mutex);
+
+	if (!(flags & PFILE_WRITE)) {
+		errno = EBADF;
+		return -1;
+	}
+
+	if (!times)
+		return vinode_file_time_set(pfp, file->vinode, NULL,
+				UTIME_MACROS_DISABLED);
+
+	struct pmemfile_time tm[2];
+
+	tm[0].sec = times[0].tv_sec;
+	tm[0].nsec = times[0].tv_nsec;
+	tm[1].sec = times[1].tv_sec;
+	tm[1].nsec = times[1].tv_nsec;
+
+	return vinode_file_time_set(pfp, file->vinode, tm,
+			UTIME_MACROS_ENABLED);
 }

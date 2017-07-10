da84910867cee597937171ac1f696ff06969bc87
Lukasz Dorau <lukasz.dorau@intel.com>
2017-07-10 12:00:02 +0200

    Add Analyzing Tool (antool)

---
+++ src/tools/antool/syscalltable.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+#
+# Copyright (c) 2017, Intel Corporation
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#
+#     * Neither the name of Intel Corporation nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from syscallinfo import *
+from utils import *
+
+
+########################################################################################################################
+# SyscallTable
+########################################################################################################################
+class SyscallTable:
+    def __init__(self):
+        self.table = []
+
+    ####################################################################################################################
+    def valid_index(self, ind):
+        if ind < len(self.table):
+            i = ind
+        else:
+            i = len(self.table) - 1
+        return i
+
+    ####################################################################################################################
+    def get(self, ind):
+        i = self.valid_index(ind)
+        return self.table[i]
+
+    ####################################################################################################################
+    def name(self, ind):
+        i = self.valid_index(ind)
+        return self.table[i].name
+
+    ####################################################################################################################
+    # read -- read the syscall table from the file
+    ####################################################################################################################
+    def read(self, path_to_syscalls_table_dat):
+        fmt = 'I4sP32sIIIiI6s6s'
+        size_fmt = struct.calcsize(fmt)
+
+        fh = open_file(path_to_syscalls_table_dat, 'rb')
+
+        size_check, = read_fmt_data(fh, 'i')
+        if size_check != size_fmt:
+            logging.error("wrong format of syscalls table file: {0:s}".format(path_to_syscalls_table_dat))
+            logging.error("      format size : {0:d}".format(size_fmt))
+            logging.error("      data size   : {0:d}".format(size_check))
+            return -1
+
+        while True:
+            try:
+                data = read_fmt_data(fh, fmt)
+                num, num_str, pname, name, length, nargs, mask, avail, nstrargs, positions, _padding = data
+                syscall = SyscallInfo(num, num_str, pname, name, length, nargs, mask, avail, nstrargs, positions)
+                self.table.append(syscall)
+
+            except EndOfFile as err:
+                if err.val > 0:
+                    logging.error("input file is truncated: {0:s}".format(path_to_syscalls_table_dat))
+                break
+
+            except:
+                logging.critical("unexpected error")
+                raise
+
+        fh.close()
+
+        return 0

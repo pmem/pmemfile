f273c7caec5090e08c42c97c4d9a998b8848a8e8
Lukasz Dorau <lukasz.dorau@intel.com>
2017-07-19 09:25:07 +0200

    antool: add tests for antool

--- tests/antool/helper_functions.sh
+++ tests/antool/helper_functions.sh
@@ -1,4 +1,3 @@
-#!/bin/bash -ex
 #
 # Copyright 2017, Intel Corporation
 #
@@ -29,23 +28,71 @@
 # THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-#
-# run-coverity.sh - runs the Coverity scan build
 #
 
-cd $WORKDIR
-cp /googletest-1.8.0.zip .
+#
+# require_superuser -- require superuser capabilities
+#
+function require_superuser() {
+	local user_id=$(sudo -n id -u)
+	[ "$user_id" == "0" ] && return
+	echo "Superuser rights required, please enter root's password:"
+	sudo date > /dev/null
+	[ $? -eq 0 ] && return
+	echo "Authentication failed, aborting..."
+	exit 1
+}
 
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DANTOOL_TESTS=SKIP
+#
+# get_line_of_pattern -- get a line number of the first pattern in the file
+#                        get_line_of_pattern <file> <pattern>
+#
+function get_line_of_pattern() {
+	grep -n -e "$2" $1 | cut -d: -f1 | head -n1
+}
 
-export COVERITY_SCAN_PROJECT_NAME="pmemfile"
-[[ "$TRAVIS_EVENT_TYPE" == "cron" ]] \
-	&& export COVERITY_SCAN_BRANCH_PATTERN="master" \
-	|| export COVERITY_SCAN_BRANCH_PATTERN="coverity_scan"
-export COVERITY_SCAN_BUILD_COMMAND="make"
+#
+# cut_part_file -- cut part of the file $1
+#                  starting from the pattern $2
+#                  ending at the pattern $3
+#
+function cut_part_file() {
 
-# Run the Coverity scan
-curl -s https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh | bash
+	local FILE=$1
+	local PATTERN1=$2
+	local PATTERN2=$3
+
+	local LINE1=$(get_line_of_pattern $FILE "$PATTERN1")
+	[ "$LINE1" == "" ] \
+		&& echo "ERROR: cut_part_file(): the start-pattern \"$PATTERN1\" not found in file $FILE" >&2 \
+		&& return
+
+	local LINE2=$(get_line_of_pattern $FILE "$PATTERN2")
+	[ "$LINE2" == "" ] \
+		&& LINE2=$(cat $FILE | wc -l) # print the file till the end
+
+	sed -n ${LINE1},${LINE2}p $FILE
+}
+
+#
+# check -- check test results (using .match files)
+#
+function check() {
+	# save output of 'match' to the file in case of error
+	local MATCH_OUT="match-${TEST_NUM}.log"
+
+	# copy match files
+	[ "$TEST_DIR" != "$(pwd)" ] && cp -v -f $TEST_DIR/*-${TEST_NUM}.log.match .
+
+	set +e
+
+	# run 'match'
+	$TEST_DIR/../match *-${TEST_NUM}.log.match >$MATCH_OUT 2>&1
+	RV=$? && [ $RV -eq 0 ] && rm -f $MATCH_OUT && return
+
+	cat $MATCH_OUT
+
+	set -e
+
+	return $RV
+}

d841425b006f39e13fdd355da68c276b81998ae2
Leszek Lugin <leszek.lugin@intel.com>
2017-09-11 14:24:25 +0200

    docker: fix setting exit code for suite script

--- utils/docker/external_tests/run-build-suite.sh
+++ utils/docker/external_tests/run-build-suite.sh
@@ -55,8 +55,7 @@ PMEMFILE_INSTALL_DIR=$HOME/pmemfile_libs
 PMEMFILE_LIB_DIR=$PMEMFILE_INSTALL_DIR/lib/pmemfile_debug
 TEST_UTILS_DIR=$WORKDIR/utils/docker/external_tests
 TEST_DIR=$HOME/testdir
-PF_DIR=$TEST_DIR/pf
-PF_POOL=$PF_DIR/pmemfile_pool
+PF_POOL=$TEST_DIR/pmemfile_pool
 MOUNTPOINT=$TEST_DIR/mountpoint
 MKFS_PMEMFILE=$PMEMFILE_INSTALL_DIR/bin/mkfs.pmemfile
 PMEMFILE_SHARED_OPTS=".. -DCMAKE_INSTALL_PREFIX=$PMEMFILE_INSTALL_DIR"
@@ -81,7 +80,6 @@ TESTS=$SUITE_UTILS_DIR/short_tests
 FAILING_TESTS=$SUITE_UTILS_DIR/failing_short_tests
 
 mkdir $TEST_DIR
-mkdir $PF_DIR
 mkdir $MOUNTPOINT
 
 # Install pmemfile
@@ -105,10 +103,17 @@ set +e
 $TEST_UTILS_DIR/run-suite.py $1 -i $SUITE_DIR -m $MOUNTPOINT -l $PMEMFILE_LIB_DIR \
 	-p $PF_POOL -t $TESTS -f $FAILING_TESTS --timeout 120 $VERBOSE
 
+
+EXIT_CODE=`echo $?`
+
 if [ "$COVERAGE" = "1" ]; then
 	bash <(curl -s https://codecov.io/bash)
 fi
 
-cd $WORKDIR
-rm -rf build
-rm -rf $TEST_DIR
+# Cleanup
+rm -r $WORKDIR/build
+rm -r $TEST_DIR
+
+if [[ "$EXIT_CODE" != "0" ]]; then
+	exit 1
+fi

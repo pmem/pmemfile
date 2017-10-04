6cac09c14c775663d1256a5a1efa4eee4f2cab48
Marcin Ślusarz <marcin.slusarz@intel.com>
2017-10-04 20:29:23 +0200

    tests: disable rw.failed_write under pmemcheck
    This test tries to create file bigger than pool size and expects this
    operation to fail.
    Some time ago this test was quick under pmemcheck because we tried to
    allocate one huge block.
    Right now blocks are limitted to 2MB, so we try to allocate this huge
    file in 2MB blocks, which means much more work and pmemcheck doesn't
    deal very well with long transactions.
    
    Just disable it under pmemcheck.

--- tests/posix/rw/rw.cmake
+++ tests/posix/rw/rw.cmake
@@ -36,7 +36,7 @@ setup()
 if(LONG_TESTS OR NOT (TRACER STREQUAL "pmemcheck"))
 	execute(${TEST_EXECUTABLE})
 else()
-	execute(${TEST_EXECUTABLE} --gtest_filter=-rw.huge_file)
+	execute(${TEST_EXECUTABLE} --gtest_filter=-rw.huge_file:rw.failed_write)
 endif()
 
 cleanup()

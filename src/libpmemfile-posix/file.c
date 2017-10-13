pmem/pmemfile

commit f60769a0891a38dd1decdbd28abdd79ec010b9ac

posix: fix creation of temporary files in directory other than /
Inspired by Coverity bug report.

src/libpmemfile-posix/file.c
@@ -208,8 +208,8 @@ _pmemfile_openat(PMEMfilepool *pfp, struct pmemfile_vinode *dir,
 	PMEMfile *file = NULL;
 
 	struct pmemfile_path_info info;
-	struct pmemfile_vinode *vinode;
-	struct pmemfile_vinode *vparent;
+	struct pmemfile_vinode *vinode = NULL;
+	struct pmemfile_vinode *vparent = NULL;
 	bool path_info_changed;
 	size_t namelen;
 	struct pmemfile_cred cred;
@@ -220,13 +220,14 @@ _pmemfile_openat(PMEMfilepool *pfp, struct pmemfile_vinode *dir,
 
 	do {
 		path_info_changed = false;
-		vparent = info.parent;
-		vinode = NULL;
 
 		if (info.error) {
 			error = info.error;
 			goto end;
 		}
+
+		vparent = vinode_ref(pfp, info.parent);
+		vinode = NULL;
 
 		namelen = component_length(info.remaining);
 
@@ -260,6 +261,7 @@ _pmemfile_openat(PMEMfilepool *pfp, struct pmemfile_vinode *dir,
 
 			/* XXX handle infinite symlink loop */
 			resolve_symlink(pfp, &cred, vinode, &info);
+			vinode_unref(pfp, vparent);
 			path_info_changed = true;
 		}
 	} while (path_info_changed);
@@ -484,6 +486,8 @@ _pmemfile_openat(PMEMfilepool *pfp, struct pmemfile_vinode *dir,
 		file->dir_pos.dir = &vinode->inode->file_data.dir;
 
 end:
+	if (vparent)
+		vinode_unref(pfp, vparent);
 	path_info_cleanup(pfp, &info);
 	cred_release(&cred);
 

tests/posix/basic/basic.cpp
@@ -479,6 +479,32 @@ TEST_F(basic, tmpfile)
 	EXPECT_TRUE(test_pmemfile_stats_match(pfp, 0, 0, 0, 0));
 }
 
+TEST_F(basic, tmpfile_not_in_root)
+{
+	pmemfile_ssize_t written;
+
+	ASSERT_EQ(pmemfile_mkdir(pfp, "/dir", 0777), 0);
+
+	PMEMfile *f = pmemfile_open(
+		pfp, "/dir", PMEMFILE_O_TMPFILE | PMEMFILE_O_WRONLY, 0644);
+	ASSERT_NE(f, nullptr) << strerror(errno);
+
+	written = pmemfile_write(pfp, f, "qwerty", 6);
+	ASSERT_EQ(written, 6) << COND_ERROR(written);
+
+	ASSERT_TRUE(test_empty_dir(pfp, "/dir"));
+
+	EXPECT_TRUE(test_pmemfile_stats_match(pfp, 2, 1, 0, 1));
+
+	pmemfile_close(pfp, f);
+
+	ASSERT_TRUE(test_empty_dir(pfp, "/dir"));
+
+	EXPECT_TRUE(test_pmemfile_stats_match(pfp, 1, 1, 0, 0));
+
+	ASSERT_EQ(pmemfile_rmdir(pfp, "/dir"), 0);
+}
+
 int
 main(int argc, char *argv[])
 {

commit f3f061c07ad429b26411358e52a68a6d66787036

posix: fix possible use after free
Reported by Coverity.
Fallout from f60769a0891a38dd1decdbd28abdd79ec010b9ac.

src/libpmemfile-posix/file.c
@@ -262,6 +262,7 @@ _pmemfile_openat(PMEMfilepool *pfp, struct pmemfile_vinode *dir,
 			/* XXX handle infinite symlink loop */
 			resolve_symlink(pfp, &cred, vinode, &info);
 			vinode_unref(pfp, vparent);
+			vinode = NULL;
 			path_info_changed = true;
 		}
 	} while (path_info_changed);